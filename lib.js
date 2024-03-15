9ff41cd364914c487952da28dea0f38ced90264a
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-15 18:32:42 +0000

    main

---
+++ lib.js
@@ -0,0 +1,15 @@
+export class CCAPI {
+
+  /**
+   * @type {}
+   */
+  ip = undefined;
+
+  /**
+   * @param {`${number}.
+   * ${number}.${number}.${number}`} ip
+   */
+  constructor(ip) {
+    this.ip = ip;
+  }
+}
