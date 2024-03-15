4bdd6adbb715408f943a1e1fe617174283095daf
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-15 21:23:39 +0000

    Init

--- lib.js
+++ lib.js
@@ -1,18 +1,29 @@
 export class CCAPI {
 
   /**
-   * @type {}
+   * @type {`${number}.${number}.${number}.${number}`}
    */
   ip = undefined;
 
   /**
-   * @param {`${number}.
-   * ${number}.${number}.${number}`} ip
+   * @param {`${number}.${number}.${number}.${number}`} ip
    */
   constructor(ip) {
     this.ip = ip;
   }
 
+  async fetch(endpoint) {
+    return fetch(`http://${this.ip}/ccapi${endpoint}`).then(async res => {
+      if(res.ok) return res.json();
+      throw new Error(await res.text())
+    });
+  }
+
+  async listFiles(storage, folder) {
+    const list = await this.fetch("/ver110/contents/sd2/" + folder);
+    return list;
+  }
 
   // http://192.168.251.75:8080/ccapi/ver110/contents/sd2/100CANON
+
 }
